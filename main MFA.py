3476ab5a73088a9650ac77b66b623f4b9ba98cd3
sunainamr <sunainam24k@gmail.com>
2024-06-08 13:02:47 +0530

    Update main cct.py

--- main MFA.py
+++ main MFA.py
@@ -1,50 +1,24 @@
-import random
+#A python program to illustrate Caesar Cipher Technique
+def encrypt(text,s):
+	result = ""
 
- #simulated database of users
-users = {"user1" : {"password": "password123", "otp_secret" : None},
- "user2" : {"password": "password1234", "otp_secret": None} }
+	# traverse text
+	for i in range(len(text)):
+		char = text[i]
 
- #function to generate and set OTP for a user
-def generate_and_set_OTP(username) :
-    users[username]["otp_secret"] = str(random.randint(10000, 99999))
-    return users[username]["otp_secret"]
+		# Encrypt uppercase characters
+		if (char.isupper()):
+			result += chr((ord(char) + s-65) % 26 + 65)
 
-#simulated OTP generator
-def generate_otp(username):
-    if users[username]["otp_secret"] is None:
-        return users[username]["otp_secret"]
+		# Encrypt lowercase characters
+		else:
+			result += chr((ord(char) + s - 97) % 26 + 97)
 
-#simulated login function
-def login(username, password, otp):
-    if username in users:
-        if password == users[username]["password"]:
-            generated_otp = generate_otp(username)
-            print(f"generated OTP for {username}: {generated_otp}")
+	return result
 
-#display otp
-        if otp == generated_otp:
-             print("login complete")
-    else:
-        print("incorrect username or password. login failed")
-
-#simulated login attempt
-def simulate_login():
-    username = input("enter username: ")
-    password = input("enter password: ")
-    otp = generate_otp(username)
-
-#generate otp before informing user
-    print(f"generated otp for {username}: {otp}")
-
-#display otp
-    otp_input = input("enter otp:")
-    login(username, password, otp_input)
-
-#main function
-    def main():
-        print("simulated multi-aunthentication system")
-        print("--------------------------------------")
-        simulate_login()
-
-    if __name__ == "__main__":
-        main()
+#check the above function
+text = "AVENGERSASSEMBLE"
+s = 4
+print ("Text : " + text)
+print ("Shift : " + str(s))
+print ("Cipher: " + encrypt(text,s))
